6bbf9ea2376f2cd27492e6205d00144bcdac20cd
AndreiZn <anznobishchev@gmail.com>
2019-09-19 18:43:54 +0300

    add mould for restructuring

---
+++ Restructured_code/functions/preICA.m
@@ -0,0 +1,10 @@
+%% PreICA script:
+% - Import data
+% - Visually inspect data (mark datasets clearly not appropriate for
+% analysis, cut beginning and end of datafiles, mark clearly bad channels)
+% - Import data to eeglab, fill in dataset info, add channel locations
+% - Rereference data
+% - Filter data
+% - Split data into epochs
+% - Perform baseline correction
+% - Reject bad trials
---
+++ Restructured_code/main.m
@@ -0,0 +1,15 @@
+% Main script - analysis of Event related potentials for the eSports
+% project
+
+CFG = []; EEG = [];
+
+%% Select files and folders
+
+%% PreICA (import, remove bad channels, rereference, filter, etc.)
+
+%% Run ICA (run ICA, save weight, save bad components)
+
+%% Level-1 analysis (within subject study)
+
+%% Level-2 analysis (group study)
+
